029350dfac2f44a7a8d1b4f75e0962d45678f421
Vishal1main <monsterr258258@gmail.com>
2025-06-12 20:31:52 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-API_ID = 12345678
-API_HASH = "10331d5d712364f57ffdd23417f4513c"
-BOT_TOKEN = "7861502352:AAFcS7xZk2NvN7eJ3jcPm_HyYh74my8vRyU"
-CHANNEL_ID = -1002178270630
-MONGO_URI = "mongodb+srv://drozmarizabel991hull:Xh89XLrFTYOPgupl@cluster0.x8qoe.mongodb.net/?retryWrites=true&w=majority"
-WEBHOOK_URL = "https://hdsp.onrender.com"  # without trailing slash
